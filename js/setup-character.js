e79e83b17515caa9bc6cf832e40850fea1cd652e
eggofevil <eggofevil@gmail.com>
2019-01-05 20:15:51 +0300

    Переименовываем некоторые объекты и и скрываем содержимое stat.js

--- js/setup-character.js
+++ js/setup-character.js
@@ -5,19 +5,19 @@
   var setupWizardCoat = document.querySelector('.setup-wizard .wizard-coat');
   var setupWizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
   var setupFireballWrap = document.querySelector('.setup-fireball-wrap');
-  var WizardCoat = {
+  var wizardCoat = {
     element: setupWizardCoat,
     inputElement: document.querySelector('[name="coat-color"]'),
     colorCounter: 0,
     colors: window.variables.COAT_COLORS
   };
-  var WizardEyes = {
+  var wizardEyes = {
     element: setupWizardEyes,
     inputElement: document.querySelector('[name="eyes-color"]'),
     colorCounter: 0,
     colors: window.variables.EYES_COLORS
   };
-  var FireBall = {
+  var fireBall = {
     element: setupFireballWrap,
     inputElement: document.querySelector('[name="fireball-color"]'),
     colorCounter: 0,
@@ -31,7 +31,7 @@
 
   var updateColor = function (styleObj) {
     var newColor = getNewColor(styleObj);
-    if (styleObj === FireBall) {
+    if (styleObj === fireBall) {
       styleObj.element.style.background = newColor;
     } else {
       styleObj.element.style.fill = newColor;
@@ -40,12 +40,12 @@
   };
 
   setupWizardCoat.addEventListener('click', function () {
-    updateColor(WizardCoat);
+    updateColor(wizardCoat);
   });
   setupWizardEyes.addEventListener('click', function () {
-    updateColor(WizardEyes);
+    updateColor(wizardEyes);
   });
   setupFireballWrap.addEventListener('click', function () {
-    updateColor(FireBall);
+    updateColor(fireBall);
   });
 })();
